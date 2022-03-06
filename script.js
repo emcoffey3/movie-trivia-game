270b2f20de3157f5370b0e72c92a4f46119bfa25
ecoffey <ecoffey@sagacom.com>
2022-03-06 17:43:18 -0500

    Initial check-in of trivia game prototype.

---
+++ script.js
@@ -0,0 +1,72 @@
+import allTriviaQuestions from './trivia-questions.js';
+
+const questionCountSpan = document.querySelector('#question-count');
+const currentScoreSpan = document.querySelector('#current-score');
+const currentQuestion = document.querySelector('#current-question');
+const answersContainer = document.querySelector('.answers-container');
+
+const GAME_QUESTION_COUNT = 5;
+let triviaQuestions = [...allTriviaQuestions];
+let score = 0;
+let questionsCount = 0;
+
+getNextTriviaQuestion();
+
+function getNextTriviaQuestion() {
+	if(questionsCount >= GAME_QUESTION_COUNT || triviaQuestions.length == 0) {
+		gameOver();
+		return;
+	}
+	const index = Math.floor(Math.random() * triviaQuestions.length);
+	const triviaQuestion = triviaQuestions[index];
+	triviaQuestions.splice(index, 1);
+	displayQuestion(triviaQuestion);
+}
+
+function displayQuestion(triviaQuestion) {
+	currentQuestion.textContent = triviaQuestion.question;
+	answersContainer.innerHTML = '';
+	triviaQuestion.answers.forEach(answer => {
+		const button = document.createElement('button');
+		button.textContent = answer.text;
+		if (answer.correct) {
+			button.dataset.correct = answer.correct;
+		}
+		button.addEventListener('click', selectAnswer);
+		answersContainer.appendChild(button);
+	});
+}
+function selectAnswer(e) {
+	questionCountSpan.innerText = ++questionsCount;
+	if (e.target.dataset.correct) {
+		e.target.classList.add('correct');
+		currentScoreSpan.innerText = ++score;
+	} else {
+		e.target.classList.add('incorrect');
+	}
+	answersContainer.querySelectorAll('button').forEach(button => {
+		button.disabled = true;
+	});
+
+	setTimeout(getNextTriviaQuestion, 1500);
+}
+
+function gameOver() {
+	let msg = `GAME OVER!\nYou scored ${score} out of ${GAME_QUESTION_COUNT}.`;
+	currentQuestion.innerText = msg;
+	answersContainer.innerHTML = '';
+	const button = document.createElement('button');
+	button.textContent = 'Play Again';
+	button.addEventListener('click', resetGame);
+	button.classList.add('reset-button');
+	answersContainer.appendChild(button);
+}
+
+function resetGame() {
+	triviaQuestions = [...allTriviaQuestions];
+	score = 0;
+	questionsCount = 0;
+	questionCountSpan.innerText = 0;
+	currentScoreSpan.innerText = 0;
+	getNextTriviaQuestion();
+}
---
+++ trivia-questions.js
@@ -0,0 +1,93 @@
+
+export default [
+	{
+		question: `In the 1988 movie Beetlejuice, after Barbara and Adam die they discover a book.  What is the book's title?`,
+		answers: [
+			{ text: `The Necronomicon`, correct: false },
+			{ text: `Tobin's Spirit Guide`, correct: false },
+			{ text: `Welcome to the After-Life`, correct: false},
+			{ text: `Handbook for the Recently Deceased`, correct: true }
+		]
+	},
+	{
+		question: `In the 2016 movie Keanu starring Keegan-Michael Key and Jordan Peele, what is Keanu?`,
+		answers: [
+			{ text: `a person`, correct: false },
+			{ text: `a car`, correct: false },
+			{ text: `a dog`, correct: false},
+			{ text: `a cat`, correct: true }
+		]
+	},
+	{
+		question: `What was the first movie based on a Marvel comic book to feature a black actor in the lead role?`,
+		answers: [
+			{ text: `Black Panther`, correct: false },
+			{ text: `Blade`, correct: true },
+			{ text: `Spawn`, correct: false},
+			{ text: `Catwoman`, correct: false }
+		]
+	},
+	{
+		question: `Actor Michael Keaton has appeared on-screen as Batman, Birdman, and what other winged comic book character?`,
+		answers: [
+			{ text: `The Falcon`, correct: false },
+			{ text: `The Vulture`, correct: true },
+			{ text: `Archangel`, correct: false},
+			{ text: `Nite Owl`, correct: false }
+		]
+	},
+	{
+		question: `In the 1957 film 12 Angry Men, what is the murder weapon?`,
+		answers: [
+			{ text: `revolver`, correct: false },
+			{ text: `switchblade`, correct: true },
+			{ text: `hammer`, correct: false},
+			{ text: `chainsaw`, correct: false }
+		]
+	},
+	{
+		question: `The character Dr. Hannibal Lecter first appeared in what film?`,
+		answers: [
+			{ text: `The Silence of the Lambs`, correct: false },
+			{ text: `Red Dragon`, correct: false },
+			{ text: `Manhunter`, correct: true },
+			{ text: `To Live and Die in LA`, correct: false }
+		]
+	},
+	{
+		question: `In the movie A Few Good Men, Tom Cruise's character Lt. Daniel Kafee is a member of which armed service branch?`,
+		answers: [
+			{ text: `Army`, correct: false },
+			{ text: `Navy`, correct: true },
+			{ text: `Marines`, correct: false },
+			{ text: `Space Force`, correct: false }
+		]
+	},
+	{
+		question: `What 2000 film about fictional serial killer Patrick Bateman was based on a novel by Bret Easton Ellis?`,
+		answers: [
+			{ text: `Summer of Sam`, correct: false },
+			{ text: `American Psycho`, correct: true },
+			{ text: `Less Than Zero`, correct: false },
+			{ text: `Saw`, correct: false }
+		]
+	},
+	{
+		question: `In the 1988 John Carpenter movie They Live starring Roddy Piper and Keith David, who are "they"?`,
+		answers: [
+			{ text: `Vampires`, correct: false },
+			{ text: `Aliens`, correct: true },
+			{ text: `Zombies`, correct: false },
+			{ text: `Werewolves`, correct: false }
+		]
+	},
+	{
+		question: `In the 1997 movie Men in Black, what actor plays Agent K?`,
+		answers: [
+			{ text: `Will Smith`, correct: false },
+			{ text: `Tommy Lee Jones`, correct: true },
+			{ text: `Tony Shalhoub`, correct: false },
+			{ text: `Ernie Hudson`, correct: false }
+		]
+	},
+];
